e8ff9123c57cafeb055331bff94a26e6cbb07a5c
ApoTheOne <anurag.saklani4@gmail.com>
2017-12-15 19:16:22 +0530

    Added services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ProductService } from './products/product.service';
 
 @Component({
   selector: 'apm-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ProductService]
 })
 export class AppComponent {
   title = 'Apo Project Management';
