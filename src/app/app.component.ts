39027c107e7b3e7e690f5234fd945728555ed523
ApoTheOne <anurag.saklani4@gmail.com>
2017-12-16 16:06:41 +0530

    Refactoring:  Added Feature Modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { ProductService } from './products/product.service';
 
 @Component({
   selector: 'apm-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [ProductService]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Apo Project Management';
